029c74f2bb809ff25a0c1abfd538386261dd735f
sabindevkota <36958825+sabindevkota@users.noreply.github.com>
2018-04-28 00:37:35 +0930

    milestone 2

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('.ham-menu');
+
+menu.addEventListener("click", function(){
+  document.querySelector('.ham-menu').classList.toggle('.ham-menu--open');
+
+});
+
+var show = document.querySelector('.clickLink');
+
+show.addEventListener("click", function(){
+
+})
